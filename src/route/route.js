3852fb08386610ab410bffb6850984324d474029
mani72507 <mani.ratna.monumamta@gmail.com>
2022-12-09 10:57:02 +0000

    All Done

---
+++ src/route/route.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const router = express.Router();
+const coinController = require("../controller/cryptoController")
+
+
+router.get("/crypto", coinController.getCrypto )
+
+
+
+
+module.exports = router;
+
